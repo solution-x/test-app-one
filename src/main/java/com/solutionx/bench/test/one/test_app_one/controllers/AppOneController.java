e69c80d6ecc79b36363fbbbf53e51f39b5272a4d
solution-x <rangaprasad7@gmail.com>
2025-08-06 10:58:15 +0800

    update AWS auth cred

--- src/main/java/com/solutionx/bench/test/one/test_app_one/controllers/AppOneController.java
+++ src/main/java/com/solutionx/bench/test/one/test_app_one/controllers/AppOneController.java
@@ -2,20 +2,18 @@ package com.solutionx.bench.test.one.test_app_one.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping(path = "")
 public class AppOneController {
+    private final static String apiOne = "testapp/one/api_one";
 
-    @PostMapping(value = "testapp/one/api_one")
-    public ResponseEntity<String> apitOne(@RequestBody String reqStr) {
+    @GetMapping(value = apiOne)
+    public ResponseEntity<String> apitOne() {
 
-        log.info("called : {}", reqStr);
-        return ResponseEntity.ok("Ack : " + reqStr);
+        log.info("called : [{}]", apiOne);
+        return ResponseEntity.ok("Ack : " + apiOne);
     }
 }
